39a5a5f80dfd4c3b7a20336de3185b6975ce7e6b
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2024-12-16 15:46:41 +0500

    Views and urls have been changed

--- root/urls.py
+++ root/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-#     path('', include('books_app.urls')),
+    path('', include('books_app.urls')),
 #     path('accounts/', include('Library_Users.urls')),
  ]
 if settings.DEBUG:
